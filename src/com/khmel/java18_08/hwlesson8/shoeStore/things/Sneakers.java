e28a98ed120de0eb8b5d6185930695396cd5f575
BogdanKhmel <bogdankhmel5@gmail.com>
2020-09-15 00:29:37 +0300

    fd

--- src/com/khmel/java18_08/hwlesson8/shoeStore/things/Sneakers.java
+++ src/com/khmel/java18_08/hwlesson8/shoeStore/things/Sneakers.java
@@ -3,7 +3,7 @@ package com.khmel.java18_08.hwlesson8.shoeStore.things;
 public class Sneakers implements Shoe {
     @Override
     public String getName() {
-        return "Sneakers";
+        return "Sneakers1";
     }
 }
 
